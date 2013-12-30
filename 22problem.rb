4931c660752b236108a32e8bc3c3200a0b95e4af
Joshua Liu <hockeyhelp6830@gmail.com>
2013-12-29 22:59:12 -0500

    finished 22

---
+++ 22problem.rb
@@ -0,0 +1,14 @@
+require "pry"
+require "awesome_print"
+final_total = 0
+
+names = File.read("problem_22_input.txt")
+array_of_names = names.gsub("\"", "").split(",")
+array_of_names.sort!
+
+array_of_names.each_with_index do |name, index|
+	name = name.chars.to_a.map! {|char| char.ord - 64 }
+	final_total += name.reduce(:+)*(index+1)
+end
+
+binding.pry
